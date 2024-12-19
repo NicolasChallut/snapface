690e733730e2de4f6d3af01e39f8f589e8443796
NicolasChallut <151745112+NicolasChallut@users.noreply.github.com>
2024-12-19 16:22:59 +0100

    modification bouton back

--- src/app/single-face-snap/single-face-snap.component.ts
+++ src/app/single-face-snap/single-face-snap.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { DatePipe, NgClass, NgStyle,  UpperCasePipe } from '@angular/common';
+import { DatePipe, NgClass, NgStyle, UpperCasePipe } from '@angular/common';
 import { FaceSnapsService } from '../services/face-snaps.service';
 import { FaceSnap } from '../models/face-snaps';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';  // Ajoutez Router ici
 
 @Component({
   selector: 'app-single-face-snap',
@@ -11,55 +11,59 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
     NgStyle,
     NgClass,
     UpperCasePipe,
-    DatePipe,
-    RouterLink    
+    DatePipe
   ],
   templateUrl: './single-face-snap.component.html',
   styleUrls: ['./single-face-snap.component.scss']
 })
 export class SingleFaceSnapComponent implements OnInit {
- faceSnap!:FaceSnap;
- snapButtonText!:string;
- userHasSnapped!: boolean;
+  faceSnap!: FaceSnap;
+  snapButtonText!: string;
+  userHasSnapped!: boolean;
 
- constructor(private faceSnapsService : FaceSnapsService,
-             private route:ActivatedRoute){}
+  constructor(
+    private faceSnapsService: FaceSnapsService,
+    private route: ActivatedRoute,
+    private router: Router  // Injection du service Router
+  ) {}
 
   ngOnInit(): void {
-  this.prepareInterface()
-  this.getFaceSnap();
-}
+    this.prepareInterface();
+    this.getFaceSnap();
+  }
 
-onSnap(): void {
-  if (this.userHasSnapped) {
-    this.unSnap();
-  } 
-  else {
-    this.snap();
+  // Méthode pour retourner à la liste des FaceSnaps
+  goBack(): void {
+    this.router.navigate(['/facesnaps']);  // Navigue vers la liste des FaceSnaps
+  }
+
+  onSnap(): void {
+    if (this.userHasSnapped) {
+      this.unSnap();
+    } else {
+      this.snap();
+    }
+  }
+
+  unSnap() {
+    this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'unsnap');
+    this.snapButtonText = 'Oh Snap!';
+    this.userHasSnapped = false;
+  }
+
+  snap() {
+    this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'snap');
+    this.snapButtonText = 'Oops, unSnap!';
+    this.userHasSnapped = true;
+  }
+
+  private getFaceSnap() {
+    const faceSnapId = this.route.snapshot.params['id'];
+    this.faceSnap = this.faceSnapsService.getFaceSnapById(faceSnapId);
+  }
+
+  private prepareInterface() {
+    this.snapButtonText = 'Oh Snap!';
+    this.userHasSnapped = false;
   }
 }
-
-unSnap() {
-  this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'unsnap');
-  this.snapButtonText = 'Oh Snap!';
-  this.userHasSnapped = false;
-}
-
-snap() {
-  this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'snap');
-  this.snapButtonText = 'Oops, unSnap!';
-  this.userHasSnapped = true;
-}
-
-private getFaceSnap(){
-  const faceSnapId = this.route.snapshot.params['id'];
-  this.faceSnap = this.faceSnapsService.getFaceSnapById(faceSnapId);
-}
-
-private prepareInterface(){
-  this.snapButtonText = 'Oh Snap!';
-  this.userHasSnapped = false;
-}
-
-}
-
